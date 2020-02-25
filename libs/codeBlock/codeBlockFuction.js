1194026fcd6421b1a273d58d50889c1e112e8c86
zhangchi <591481308@qq.com>
2020-02-25 13:30:28 +0800

    Site updated: 2020-02-25 13:30:26

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,3 +1,5 @@
-// build time:Tue Feb 25 2020 13:23:24 GMT+0800 (GMT+08:00)
-$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
-//rebuild by neat 
+// 代码块功能依赖
+
+$(function () {
+    $('pre').wrap('<div class="code-area" style="position: relative"></div>');
+});
